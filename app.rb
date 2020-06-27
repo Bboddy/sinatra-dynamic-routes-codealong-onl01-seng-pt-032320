97df3a18904f502879e36bab4947f06777458607
Bryce B <bryce.boddy@gmail.com>
2020-06-27 05:24:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,18 +13,3 @@ end
 
 
   
-  get '/reversename/:name' do
-    @str = params[:name]
-    "#{str.reverse}"
-  end
-  
-  get "/square/:number" do
-    "#{params[:number].to_i*params[:number].to_i}"
-  end
-  
-  get "/say/:number/:phrase" do
-    @x = params[:number].to_i
-    @s = ""
-    x.times {@s += params[:phrase]}
-    "#{s}"
-  end
